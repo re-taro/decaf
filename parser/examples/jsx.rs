520802f5f265b63ece98d073ee7f6a7094d1d5ce
Rintaro Itokawa <me@re-taro.dev>
2024-07-18 02:00:00 +0900

    feat: initialize

---
+++ parser/examples/jsx.rs
@@ -0,0 +1,18 @@
+use decaf_parser::{ASTNode, JSXRoot, ParseOutput, SourceId, ToStringSettingsAndData};
+
+fn main() {
+	let source = "<Layout> <p>My page content, wrapped in a layout!</p> </Layout>";
+	let ParseOutput(result, state) = JSXRoot::from_string(
+		source.to_owned(),
+		Default::default(),
+		SourceId::NULL,
+		None,
+		Vec::new(),
+	)
+	.unwrap();
+
+	println!(
+		"{}",
+		result.to_string(&ToStringSettingsAndData(Default::default(), state.function_extractor))
+	);
+}
---
+++ parser/examples/statement.rs
@@ -0,0 +1,12 @@
+use decaf_parser::{ASTNode, SourceId, Statement};
+
+fn main() {
+	let statement = Statement::from_string(
+		"const x = 4".to_owned(),
+		Default::default(),
+		SourceId::NULL,
+		None,
+		Vec::new(),
+	);
+	println!("{:#?}", statement);
+}
