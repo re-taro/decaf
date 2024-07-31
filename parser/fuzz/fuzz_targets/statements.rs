d028b4fe151e77eb977219ef3213db1f5b4eb4a9
Rintaro Itokawa <me@re-taro.dev>
2024-07-31 22:43:20 +0900

    refactor: return Corpus:Keep and Corpus:Reject instead of ()

--- parser/fuzz/fuzz_targets/statements.rs
+++ parser/fuzz/fuzz_targets/statements.rs
@@ -3,16 +3,16 @@
 use std::str;
 
 use decaf_parser::{ASTNode, Module, ParseOutput, SourceId, ToStringSettingsAndData};
-use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{fuzz_target, Corpus};
 use pretty_assertions::assert_eq;
 
-fn do_fuzz(data: &str) {
+fn do_fuzz(data: &str) -> Corpus {
 	let input = data.trim_start();
 
 	let Ok(ParseOutput(module, state)) =
 		Module::from_string(input.to_owned(), Default::default(), SourceId::NULL, None, Vec::new())
 	else {
-		return ();
+		return Corpus::Reject;
 	};
 
 	let output1 =
@@ -25,15 +25,17 @@ fn do_fuzz(data: &str) {
 		None,
 		Vec::new(),
 	) else {
-		return ();
+		return Corpus::Reject;
 	};
 
 	let output2 =
 		module.to_string(&ToStringSettingsAndData(Default::default(), state.function_extractor));
 
 	assert_eq!(output1, output2);
+
+	return Corpus::Keep;
 }
 
 fuzz_target!(|data: &str| {
-	let _ = do_fuzz(data);
+	do_fuzz(data);
 });
