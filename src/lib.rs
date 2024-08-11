756acd71031157ee686b14532ea624d046c91810
Rintaro Itokawa <me@re-taro.dev>
2024-08-11 23:25:30 +0900

    feat: add temp build command, js-cli and support WASM

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+mod ast_explorer;
+mod error_handling;
+mod temp;
+pub(crate) mod utilities;
+
+pub mod cli;
+
+#[cfg(target_family = "wasm")]
+mod wasm_bindings;
+
+#[cfg(target_family = "wasm")]
+pub use wasm_bindings::build_wasm as build;
+
+pub use cli::run_cli;
