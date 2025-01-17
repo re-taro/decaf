3105fb856f7a28d851fe9fcce0fe0f91ac600a4d
Rintaro Itokawa <me@re-taro.dev>
2025-01-17 23:47:31 +0900

    feat: wrote paper (#27)
    * feat: initialaize paper
    
    * feat: wrote specification.tex [WIP]
    
    * feat: wrote impl section [WIP]
    
    * feat: add outline layout
    
    * feat: wrote about impl
    
    * feat: wrote experiment section
    
    * feat: change title
    
    * feat: wrote experiment.tex [WIP]
    
    * feat: update figures
    
    * feat: wrote experiment.tex
    
    * feat: wrote overview.tex
    
    * fix: remove 型と集合 section
    
    * feat: wrote summary.tex and appendix.tex
    
    * feat: wrote abstract
    
    * fix: rename my teacher
    
    * fix: fix ?'s font
    
    * feat: change theme and fix abstract
    
    * feat: initialize proceeding
    
    * feat: wrote proceeding
    
    * feat: wrote proceeding
    
    * feat: wrote proceeding
    
    * feat: update section title
    
    * feat: update experiment
    
    * feat: fix paper

---
+++ paper/paper/bin/linter-tex-fmt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FILES=$(find ./ -type f \( -name "*.tex" \) -not -name '__latexindent_temp.tex')
+# FILES=$(find ./ -type f \( -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" \) -not -name '__latexindent_temp.tex')
+for file in $FILES;do
+    tex-fmt $file
+done
